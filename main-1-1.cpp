73b3d67c8913a4324880973a6b9c066e6b620179
nfayazi <a1886407@adelaide.edu.au>
2023-07-26 14:30:57 +0930

    code inside main function

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 using namespace std;
+extern int array_sum(int array[], int n);
 
 int main(){
 int n;
@@ -14,9 +15,7 @@ for (int i = 0; i < n; i++){
     cout << "What number is in the array?: " << endl;
     cin >> array[i];
 }
+array_sum(int array[n], int n);
 
-int array_sum();
- int test;
- 
     return 0;
 }
