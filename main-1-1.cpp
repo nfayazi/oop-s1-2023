9aca26dcd9afd59fc3aba2768e00f16b994d68ef
nfayazi <a1886407@adelaide.edu.au>
2023-07-26 14:47:55 +0930

    added extra line

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -6,7 +6,8 @@ extern int array_sum(int array[], int n);
 int main(){
     int elements[5] = {1,2,3,4,5};
 
-cout << "The sum is: " << array_sum(elements, 5);
+cout << "The sum is: " << array_sum(elements, 5) << endl;
+cout < endl;
 
     return 0;
 }
