255206ac92edbb2325e965b47620e03cdc3c7c6a
Nazia Fayazi <naziafayazi@student-10-201-20-224.wireless.adelaide.edu.au>
2023-07-26 14:11:26 +0930

    main file code

---
+++ main-1-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int n;
+
+cout << "How big is the array?: " << endl;
+cin >> n;
+
+int array[n];
+
+for (int i = 0; i < n; i++){
+    cout << "What number is in the array?: " << endl;
+    cin >> array[i];
+}
+
+int array_sum();
+
+    return 0;
+}
