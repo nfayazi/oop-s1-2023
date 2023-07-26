de2685be8a6f3ee29eb32871864791d87a43c48d
nfayazi <a1886407@adelaide.edu.au>
2023-07-26 20:43:24 +0930

    changed to add desired number into count

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -6,7 +6,7 @@
 int count_evens(int number) {
   int even_count = 0;
 
-  for (int i = 1; i < number; i++) {
+  for (int i = 1; i <= number; i++) {
     if (i % 2 == 0) {
       even_count++;
     }
