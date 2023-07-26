1c4c9b23e3c82052703f48ad1b1a62c730245781
nfayazi <a1886407@adelaide.edu.au>
2023-07-26 20:40:01 +0930

    modified print message

---
+++ main-1-5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// 1-5. Using a for loop, count the number of even numbers between 1 and a
+// number we supply (inclusive). Return 0 if the parameter is less than 1.
+using namespace std;
+
+extern int count_evens(int number);
+
+int main() {
+  int number;
+  cout << "Enter the desired number: ";
+  cin >> number;
+
+  cout << "There are " << count_evens(number) << "even numbers between 1 and "
+       << number << ".";
+  cout << endl;
+
+  return 0;
+}
