a2532955eb47b680f353efe2e46c28de916ad813
nfayazi <a1886407@adelaide.edu.au>
2023-07-26 20:39:34 +0930

    initial 1-5 files commit

---
+++ function-1-5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// 1-5. Using a for loop, count the number of even numbers between 1 and a
+// number we supply (inclusive). Return 0 if the parameter is less than 1.
+
+int count_evens(int number) {
+  int even_count;
+
+  for (int i = 1; i < number; i++) {
+    if (i % 2 == 0) {
+      even_count++;
+    }
+  }
+
+  return even_count;
+}
