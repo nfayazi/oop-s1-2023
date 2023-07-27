05b1adb52a216a04d83be1172a4ae55d7ccc93a9
nfayazi <a1886407@adelaide.edu.au>
2023-07-27 17:15:13 +0930

    initial 2-3 files commit

---
+++ function-2-3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+// 2-3. Write a function that given an array of integers and the length of the
+// array, will count the amount of times the numbers 2, 5 and 9 appear using a
+// switch statement. The function must print out your results on one line in the
+// following form:
+//       2:<num_twos>;5:<num_fives>;9:<num_nines>;
+
+// If the array contained three 2s, one 5 and eleven 9s, the output would be:
+//       2:3;5:1;9:11;
+
+void two_five_nine(int array[], int n) {
+int counter2 = 0;
+int counter5 = 0;
+int counter9 = 0;
+
+for (int i = 0; i < n; i++){
+    if (n < 1){
+        break;
+    } else if (array[i] == 2){
+        counter2++;
+    } else if (array[i] == 5){
+        counter5++;
+    } else if (array[i] == 9){
+        counter9++;
+    }
+}
+
+cout << "2:" << counter2 << ";5:" << counter5 << ";9:" << counter9 << endl;
+
+}
---
+++ main-2-3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+// 2-3. Write a function that given an array of integers and the length of the
+// array, will count the amount of times the numbers 2, 5 and 9 appear using a
+// switch statement. The function must print out your results on one line in the
+// following form:
+//       2:<num_twos>;5:<num_fives>;9:<num_nines>;
+
+// If the array contained three 2s, one 5 and eleven 9s, the output would be:
+//       2:3;5:1;9:11;
+
+extern void two_five_nine(int array[], int n);
+
+int main(){
+    int n;
+    cout << "How many elements are there? ";
+    cin >> n;
+
+int array[n];
+
+for (int i = 0; i < n; i++){
+    cout << "Enter a number: ";
+    cin >> array[i];
+}
+
+
+two_five_nine(array, n);
+
+    return 0;
+}
