82726edba70d5a8371bc7ca1cb24ea7fccb897e2
nfayazi <a1886407@adelaide.edu.au>
2023-07-28 16:59:45 +0930

    initial commit 3-5

---
+++ function-3-5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+// 3-5. Write a function that returns the sum of the elements in the even
+// positions in an array. The function must return 0 if the size parameter, n,
+// is less than 1. Note, 0 is considered an even position.
+
+double sum_even(double array[], int n) {
+int sum = 0;
+
+if (n < 1){
+    return 0;
+}
+
+
+for (int i = 0; i < n; i++){
+    if (i % 2 == 0){
+        sum = sum + array[i];
+    }
+    }
+
+    return sum;
+}
+
+
+
+
+
+}
