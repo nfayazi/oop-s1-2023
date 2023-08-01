6d922022c4217cc3755af8e313d7382ff8fd82ae
nfayazi <a1886407@adelaide.edu.au>
2023-08-01 21:03:02 +0930

    printing only decimal of subtraction

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -1,98 +1,81 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main() {
+  int digits = 4;
 
-int digits = 4;
+  int binary1[4] = {1, 1, 1, 0};
+  int reverse_binary1[4];
+  int decimal1 = 0;
 
-int binary1[4] = {1, 0, 1, 0};
-int reverse_binary1[4];
-int decimal1 = 0;
+  int binary2[4] = {1, 0, 1, 0};
+  int reverse_binary2[4];
+  int decimal2 = 0;
 
-int binary2[4] = {0, 1, 1, 1};
-int reverse_binary2[4];
-int decimal2 = 0;
+  int new_binary[digits];
+  int reverse_new[4];
+  int new_decimal = 0;
 
-int new_binary[digits];
-int reverse_new[4];
-int new_decimal = 0;
+  int i;
 
-int i;
+  // initialise the reverse of each binary array
+  for (int i = 0; i < digits; i++) {
+    reverse_binary1[i] = binary1[digits - 1 - i];
+  }
 
-// initialise the reverse of each binary array
-    for (int i = 0; i < digits; i++) {
-        reverse_binary1[i] = binary1[digits - 1 - i];
+  for (int i = 0; i < digits; i++) {
+    reverse_binary2[i] = binary2[digits - 1 - i];
+  }
+
+
+  for (i = digits; i >= 0; i--) {
+    if (binary1[i] == 0 && binary2[i] == 1) {
+      new_binary[i] = 1;
+      binary1[i - 1] = 0;
+    } else if (binary1[i] == 1 && binary2[i] == 0) {
+      new_binary[i] = 1;
+    } else if (binary1[i] == 0 && binary2[i] == 0) {
+      new_binary[i] = 0;
     }
+  }
 
-    for (int i = 0; i < digits; i++) {
-        reverse_binary2[i] = binary2[digits - 1 - i];
-    }
+  for (int i = 0; i < digits; i++) {
+    reverse_new[i] = new_binary[digits - 1 - i];
+  }
 
-for (i = digits - 1; i >= 0; i--){
-    if (binary1[i] == 0 && binary2[i] == 1){
-        new_binary[i] = 1;
-        binary1[i - 1] = 0;
-    } else if (binary1[i] == 1 && binary2[i]){
-        new_binary[i] = 1;
+  // perform subtraction on original binary
+  for (i = 0; i < digits; i++) {
+    if (reverse_binary1[i] == 1) {
+      decimal1 += pow(2, i);
     }
-}
+    if (reverse_binary2[i] == 1) {
+      decimal2 += pow(2, i);
+    }
+    if (reverse_new[i] == 1) {
+      new_decimal += pow(2, i);
+    }
+  }
 
-    for (int i = 0; i < digits; i++) {
-        reverse_new[i] = new_binary[digits - 1 - i];
-    }
-
-
-//perform subtraction on original binary
-for (i = 0; i < digits; i++){
-    if (reverse_binary1[i] == 1){
-        decimal1 += pow(2,i);
-    }
-    if (reverse_binary2[i] == 1){
-        decimal2 += pow(2, i);
-    }
-    if (new_binary[i] == 1){
-        new_decimal += pow(2, i);
-    }
-}
-
-//print digits of binary1
-cout << "The digits for binary1 are: ";
-for (i = 0; i < digits; i++){
+  // print digits of binary1
+  cout << "The digits for binary1 are: ";
+  for (i = 0; i < digits; i++) {
     cout << binary1[i];
-}
-cout << endl;
+  }
+  cout << endl;
 
-
-//print digits for binary2
-cout << "The digits for binary2 are: ";
-for (i = 0; i < digits; i++){
+  // print digits for binary2
+  cout << "The digits for binary2 are: ";
+  for (i = 0; i < digits; i++) {
     cout << binary2[i];
-}
-cout << endl;
+  }
+  cout << endl;
 
-cout<< "The decimal value of binary1 is " << decimal1 << " and the decimal for binary2 is " << decimal2 << endl;
+  cout << "The decimal value of binary1 is " << decimal1
+       << " and the decimal for binary2 is " << decimal2 << endl;
 
-cout<< "The binary of ";
-for (i = digits - 1; i >= 0; i--){
-    cout << reverse_binary1[i];
-}
-cout<< " - ";
+  cout << "The value of binary1 - binary2 is " << new_decimal << endl;
 
-for (i = digits - 1; i >= 0; i--){
-    cout << reverse_binary2[i];
-}
-
-cout << " is ";
-for (i = digits; i >= 0; i--){
-    cout << reverse_new[i];
-}
-
-cout << endl;
-cout << "the decimal value of this new binary is " << new_decimal << endl;
-
-
-    return 0;
-
+  return 0;
 }
