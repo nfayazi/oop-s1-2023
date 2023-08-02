de55af66f95daaa21cf62f0b4c90f499216b08f6
nfayazi <a1886407@adelaide.edu.au>
2023-08-02 21:05:38 +0930

    print out sum

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -8,14 +8,13 @@ using namespace std;
 int sum_diagonal(int array[4][4]) {
     int sum = 0;
 
-    for (int r = 0; r < sizeof(array, 1); r++){
-        for (int c = 0; r < sizeof(array, 2); c++ ){
+    for (int r = 0; r < 4; r++){
+        for (int c = 0; c < 4; c++){
             if (r == c){
                 sum += array[r][c];
             }
         }
     }
-    
     
      return sum; 
      
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -5,24 +5,20 @@ using namespace std;
 // 1-1. Given a matrix of integers, return the sum of the elements along the
 // main diagonal (top left bottom right)
 
-int sum_diagonal(int array[4][4]);
+extern int sum_diagonal(int array[4][4]);
 
 int main() {
   int array[4][4];
+  const int size = 4;
   cout << "Enter the elements for the array: " << endl;
 
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
       cin >> array[i][j];
     }
   }
 
-  cout << "This matrix is: " << endl;
-  for (int i = 0; i < 4; i++) {
-    for (int j = 0; j < 4; j++) {
-      cin >> array[i][j];
-    }
-  }
+cout << "the diagonal sum of this matrix is: " << sum_diagonal(array) << endl;
 
   return 0;
 }
