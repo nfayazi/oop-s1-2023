24796c82a1fbd4d677aa4ef596c5256f7c7c6959
nfayazi <a1886407@adelaide.edu.au>
2023-08-04 16:35:52 +0930

    initial 2-3 files

---
+++ main-2-3.cpp
@@ -0,0 +1,15 @@
+
+/*
+
+2-3. Given an array of integers, write a function to calculate the sum of the
+elements if it is a palindrome array. If it is not a palindrome array, your
+function must return -2. Your function must call separate functions to check
+whether or not the array is a palindrome and to calculate the sum of its
+elements. If the length is O or negative each function must return -1 or false
+as its result.
+
+*/
+
+int sum_if_palindrome(int integers[], int length) {}
+bool is_palindrome(int integers[], int length){}
+int sum_array_elements(int integers[], int length){}
