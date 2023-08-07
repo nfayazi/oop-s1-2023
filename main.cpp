dd112b4a30f594f8db53fcf6b6515b7b88a1099d
nfayazi <a1886407@adelaide.edu.au>
2023-08-07 20:35:40 +0930

    q7

--- main.cpp
+++ main.cpp
@@ -1,46 +1,41 @@
 #include <iostream>
+
 #include "workshop.hpp"
 using namespace std;
 
-int main(){
-    double value = 2;
-    double* val = &value;
-    cout << "Part 2:" << endl;
-    cout << "The value is now: ";
-    changeValue(val);
-    cout << endl;
+int main() {
+  double value = 2;
+  double* val = &value;
+  cout << "Part 2:" << endl;
+  cout << "The value is now: ";
+  changeValue(val);
+  cout << endl;
 
+  cout << "Part 3:" << endl;
+  double a[3] = {1, 2, 3};
+  int size = 3;
+  cout << "The array is: ";
+  printArray(a, size);
+  cout << endl;
 
-    cout << "Part 3:" << endl;
-    double a[3] = {1, 2, 3};
-    int size = 3;
-    cout << "The array is: ";
-    printArray(a, size); 
-    cout << endl;
+  cout << "Part 4:" << endl;
+  cout << "The largest element in a is: " << arrayMax(a, size) << endl;
 
+  cout << "Part 6: " << endl;
+  int N = 3;
+  double M = 12;
+  double* array = dynamicArray(N, M);
+  cout << "The max of this array is: " << arrayMax(a, N) << endl;
+  delete[] array;
 
-    cout << "Part 4:" << endl;
-    cout << "The largest element in a is: " << arrayMax(a, size) << endl;
+  cout << "Part 7: " << endl;
+  double* seven = dynamicArray(N, M);
 
-cout << "Part 6: " << endl;
-int N = 3;
-double M = 12;
-double* array = dynamicArray(N, M);
-cout << "The max of this array is: " << arrayMax(a, N) << endl;
-delete[] array;
+  cout << "The elements of the array are: " << endl;
+  for (int i = 0; i < N; i++) {
+    cout << seven[i] << endl;
+  }
+  delete[] seven;
 
-
-    cout << "Part 7: " << endl;
-    double* seven = dynamicArray(N, M);
-
-cout << "The elements of the array are: " << endl;
-    for (int i = 0; i < N; i++){
-        cout << seven[i] << endl;
-    }
-    delete[] seven;
-
-return 0;
+  return 0;
 }
-
-
-
