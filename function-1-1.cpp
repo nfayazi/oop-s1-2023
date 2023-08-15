dfe68fc83c09df20cd68c9079fb501c231ef5505
nfayazi <a1886407@adelaide.edu.au>
2023-08-15 19:10:28 +0930

    1-1

---
+++ function-1-1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+/*
+
+1-1. You must write two functions. The first function, readNumbers, is to read a
+set of 10 numbers from std::cin and use them to initialise a dynamically
+allocated array of int of length 10. The function must return a pointer to the
+array. You may assume that the input contains 10 integer values and nothing
+else. The second function, printNumbers, takes an int array of any length
+greater than 0 and prints all numbers in the array, one per line. The numbers
+must be printed in order starting with the first entry. Each line of output
+should start with the array index of the number being written out, followed by a
+space, then the number and finally a newline. For example, if the numbers 5 2 3
+4 35 6 7 8 -9 10 are read by the program, the output should be:
+
+0 5
+1 2
+2 3
+3 4
+4 35
+5 6
+6 7
+7 8
+8 -9
+9 10
+
+*/
+
+int *readNumbers(){
+int *numbers = new int(10);
+    cout << "Enter the numbers: " << endl;
+    for (int i = 0; i < 10; i++){
+        cin >> *(numbers + i);
+    }
+    return numbers;
+}
+void printNumbers(int *numbers,int length){
+
+    for (int i = 0; i < length; i++){
+        cout << i << " " << *(numbers + i) << endl;
+     }
+     
+}
