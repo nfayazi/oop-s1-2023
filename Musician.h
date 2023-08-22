6df00ae73b08fed5ca07f9f559cf26a659709a06
nfayazi <a1886407@adelaide.edu.au>
2023-08-22 20:13:13 +0930

    .

--- Musician.h
+++ Musician.h
@@ -13,25 +13,21 @@ class Musician {
     int yearsExperience;
 
     public:
-void setInstrument(string instrument){}
-void setYearsExperience(int experience) {}
+void setInstrument(string instrument){
+    Instrument = instrument;
 }
-int get_experience(){ return yearsExperience;};
+void setYearsExperience(int yearsOfExperience) {
+    yearsExperience = yearsOfExperience;
+}
+// int get_experience(){ return yearsExperience;};
 Musician() {
-    Name = "Unknown";
     Instrument = "null";
     yearsExperience = 0;
-}
-*/
-Musician(string name, string instrument, int experience) {
-    Name = name;
-    Instrument = instrument;
-    yearsExperience = experience;
 };
-/*
+
 Musician(string instrument, int experience) {
     Instrument = instrument;
     yearsExperience = experience;
 };
-*/
+
 };
