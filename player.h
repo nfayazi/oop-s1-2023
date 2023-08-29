7a110bc328186f95e380c2a739286001b96c0805
nfayazi <a1886407@adelaide.edu.au>
2023-08-29 23:55:38 +0930

    game

---
+++ player.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class player {
+    private:
+    string Name;
+    int Health;
+    int Damage;
+
+    public:
+    player(string name, int health, int damage);
+    void attack(player* opponent, int damage);
+    void takeDamage(int damage);
+    int getHealth();
+    void setHealth(int health);
+    int getDamage();
+    void setDamage(int damage);
+};
