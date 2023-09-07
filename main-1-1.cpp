997e78e66d16db26ec7da44e430a2e0effa22fd1
nfayazi <a1886407@adelaide.edu.au>
2023-09-07 13:25:59 +0930

    1-1

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main(){
     Car car1 = Car(20);
-    car1.drive(10);
     cout << "Car 1: " << endl;
     cout << "Price: " << car1.get_price() << " | ";
     cout << "Emissions: " << car1.get_emissions() << endl;
-
+    car1.drive(10);
+    cout << "Emissions now: " << car1.get_emissions() << endl;
 
     return 0;
 }
