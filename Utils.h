cbc9a0627ab78fc18b337b4e88eca32127f107d5
nfayazi <a1886407@adelaide.edu.au>
2023-10-24 19:38:07 +1030

    no

---
+++ Utils.h
@@ -0,0 +1,25 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <tuple>
+
+class Utils {
+
+protected:
+
+
+
+public:
+std::tuple<int, int> generateRandomPos(int gridWidth, int gridHeight)
+{
+    
+}
+
+double calculateDistance(std::tuple<int, int> pos1, std::tuple<int, int> pos2)
+{
+
+}
+
+
+};
+
+#endif // UTILS_H
