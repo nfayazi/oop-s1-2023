33a6ef0485ef3dd8c0e4f7f6c9c599d6ee1b4026
nfayazi <a1886407@adelaide.edu.au>
2023-10-26 14:54:03 +1030

    S

--- Persona.h
+++ Persona.h
@@ -12,12 +12,8 @@ class Persona : public Spot {
 
     std::tuple<int, int> shift(int dx, int dy)
     {
-        std::tuple<int, int> loc1;
-        std::tuple<int, int> loc2;
-        dx = std::get<0>(loc1) - std::get<0>(loc2);
-        dy = std::get<1>(loc1) - std::get<1>(loc2);
-            location = std::make_tuple(dx, dy);
-            return location;
+    location = make_tuple(get<0>(location) + dx, get<1>(location) + dy);
+    return location;
     }
 
 
