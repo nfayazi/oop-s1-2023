094eb5a30139cc631c8376df2769266056c383d2
nfayazi <a1886407@adelaide.edu.au>
2023-10-26 14:37:56 +1030

    main 1

--- main-1.cpp
+++ main-1.cpp
@@ -2,9 +2,26 @@
 #include "Influence.h"
 #include "Assists.h"
 
-int main(){
-    Spot spot(2, 3, 't');
+#include <tuple>
+#include <iostream>
+using namespace std;
 
+int main(){
+    Assists assist;
+
+    int matrixWidth = 10;
+    int matrixHeight = 10;
+
+    // Generate random positions
+    tuple<int, int> loc1 = assist.createRandomLoc(matrixWidth, matrixHeight);
+    tuple<int, int> loc2 = assist.createRandomLoc(matrixWidth,matrixHeight);
+
+    // Calculate distance between positions
+    double distance = assist.evaluateDistance(loc1, loc2);
+
+    cout << "Location 1: (" << get<0>(loc1) << ", " << get<1>(loc1) << ")" << endl;
+    cout << "Location 2: (" << get<0>(loc2) << ", " << get<1>(loc2) << ")" << endl;
+    cout << "Distance between positions: " << distance << endl;
 
     return 0;
 }
