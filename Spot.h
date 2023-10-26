91f5aed672502cce2f153b78673ac2b8c2f0f709
nfayazi <a1886407@adelaide.edu.au>
2023-10-26 14:47:26 +1030

    kms

--- Spot.h
+++ Spot.h
@@ -8,15 +8,15 @@ class Spot {
   char category;
 
  public:
-  Spot(int x, int y, char category) {
+  Spot(int x, int y, char Category) {
     location = std::make_tuple(x, y);
-    category = category;
+    category = Category;
   }
   virtual ~Spot() {}
   std::tuple<int, int> getLoc() { return location; }
   char getCategory() { return category; }
   void setLoc(int x, int y) { location = std::make_tuple(x, y); }
-  void setCategory(char category) { category = category; }
+  void setCategory(char Category) { category = Category; }
 };
 
 #endif  // SPOT_H
--- main-1.cpp
+++ main-1.cpp
@@ -14,12 +14,9 @@ int main(){
     int matrixWidth = 10;
     int matrixHeight = 10;
 
-
-    // Generate random positions
     tuple<int, int> loc1 = assist.createRandomLoc(matrixWidth, matrixHeight);
     tuple<int, int> loc2 = assist.createRandomLoc(matrixWidth,matrixHeight);
 
-    // Calculate distance between positions
     double distance = assist.evaluateDistance(loc1, loc2);
 
     cout << "Location 1: (" << get<0>(loc1) << ", " << get<1>(loc1) << ")" << endl;
